177d30cf6c6587a95876debc3dac531b6ead5777
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-17 00:12:48 +0500

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import connectDB from "./db.js";
 import cors from "cors";
 import resumeRoutes from "./routes/resume.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 connectDB();
@@ -9,10 +11,17 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ===== FRONTEND (ROOT index.html) =====
+app.use(express.static(path.join(__dirname, "../../")));
+
 app.get("/", (req, res) => {
-  res.send("API ishlayapti 🚀");
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
+// ===== API =====
 app.use("/api/resume", resumeRoutes);
 
 const PORT = process.env.PORT || 5000;
