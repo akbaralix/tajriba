1cdd337e42408e2017e69e9ec22989fc160a2df5
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-16 23:58:28 +0500

    Update db.js

--- src/server/db.js
+++ src/server/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE); // eski opsiyalarni olib tashladik
+    await mongoose.connect(
+      "mongodb+srv://tursunboyevakbarali807_db_user:fBuEm29mZLPpZgKq@cluster0.rcjkua0.mongodb.net/?appName=Cluster0"
+    );
     console.log("MongoDB ulanishi muvaffaqiyatli");
   } catch (error) {
     console.error("MongoDB ulanishda xatolik:", error);
