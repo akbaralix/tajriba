967f5a75e900f91124de673de8b98a475e95fde4
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-19 10:56:51 +0500

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -13,6 +13,9 @@ app.use(express.json());
 // Resume route
 app.use("/api/resume", resumeRoutes);
 app.use("/api/order", orderRoutes);
+app.get("/", (req, res) => res.send("Server OK"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server ${PORT} portda ishlayapti`));
+
