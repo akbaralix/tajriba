7304799455d95059531d872dfc02ad9e7e6533c7
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-19 10:51:26 +0500

    Refactor MongoDB connection in db.js
    Updated MongoDB connection string and removed dotenv configuration.

--- src/server/db.js
+++ src/server/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE); // eski opsiyalarni olib tashladik
+    await mongoose.connect(
+      "mongodb+srv://tursunboyevakbarali807_db_user:fBuEm29mZLPpZgKq@cluster0.rcjkua0.mongodb.net/yourDBname"
+    );
     console.log("MongoDB ulanishi muvaffaqiyatli");
   } catch (error) {
-    console.error("MongoDB ulanishda xatolik:", error);
+    console.error("MongoDB ulanishda xatolik:", error.message);
     process.exit(1);
   }
 };
