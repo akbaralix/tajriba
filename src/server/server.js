ef647484def1252d983ad816f939432b556d69e6
Akbarali Tursunboyev <tursunboyevakbarali807@gmail.com>
2025-12-19 11:24:30 +0500

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -7,21 +7,26 @@ import orderRoutes from "./routes/order.js";
 
 const app = express();
 
+// MongoDB bilan ulanish
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// API routes
 app.use("/api/resume", resumeRoutes);
 app.use("/api/order", orderRoutes);
 
-
+// === FRONTEND STATIC FAYLLAR ===
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "dist")));
 
+// Barcha GET so‘rovlarni dist/index.html ga yo‘naltirish
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
+// Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server ${PORT} portda ishlayapti`));
